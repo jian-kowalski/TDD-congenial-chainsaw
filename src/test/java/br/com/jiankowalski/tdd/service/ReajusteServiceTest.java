39aacc8d8c6b38c8f5c37bca46e6bf17fb685b03
jian-kowalski <jian_kowalski@hotmail.com>
2021-06-23 18:12:52 -0300

    Organização de imports

--- src/test/java/br/com/jiankowalski/tdd/service/ReajusteServiceTest.java
+++ src/test/java/br/com/jiankowalski/tdd/service/ReajusteServiceTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
